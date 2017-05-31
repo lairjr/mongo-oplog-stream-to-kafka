bbc2511aee87f2f88872129d4b2adfa989d26997
lairjr <lairjr@outlook.com>
2017-05-31 15:28:25 -0300

    Add docker compose

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+
+const oplogMongodbUri = 'mongodb/local';
+
+mongoose.connect(oplogMongodbUri, {
+  poolSize: 10,
+  socketOptions: { keepAlive: 250 },
+}).then(() => {
+  console.log('connected');
+}).catch((err) => {
+  console.log(err);
+});
